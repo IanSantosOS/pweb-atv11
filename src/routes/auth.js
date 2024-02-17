0db857ea7ef9b63a3c71fe24772ffdbef794c1a0
Ian Santos <iann.silva2006@gmail.com>
2024-02-17 18:13:02 -0300

    logout working

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -5,7 +5,17 @@ const formValidation = require('../middlewares/formValidation');
 const accountsController = require('../controllers/accountsController');
 
 router.post('/login', formValidation.loginValidation, accountsController.loginUser);
+
 router.post('/signup', formValidation.signupValidation, accountsController.createUser);
-router.post('/logout', (req, _res) => req.session.login = undefined);
+
+router.post('/logout', (req, res) => {
+  req.session.destroy(err => {
+    if (err) {
+      res.status(500).send('Erro ao tentar sair da conta');
+    } else {
+      res.redirect('/');
+    }
+  });
+});
 
 module.exports = router;
